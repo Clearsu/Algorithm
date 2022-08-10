05be77b18e8488398bcaa7f36954ffe4d404faba
jincpark <clearwater1219@gmail.com>
2022-08-11 02:46:51 +0900

    added

--- b2447.c
+++ b2447.c
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 11:10:20 by jincpark          #+#    #+#             */
-/*   Updated: 2022/07/30 11:30:12 by jincpark         ###   ########.fr       */
+/*   Updated: 2022/08/01 14:27:11 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*
@@ -17,31 +17,32 @@
 
 
 
+
+
 #include <stdio.h>
 
-void	print_stars(int i, int j, int n)
+void    print_stars(int i, int j, int n)
 {
-	if ((i / n) % 3 == 1 && (j / n) % 3 == 1)
-		printf(" ");
-	else
-	{
-		if (n / 3 == 0)
-			printf("*");
-		else
-			print_stars(i, j, n / 3);
-	}
+    if ((i / n) % 3 == 1 && (j / n) % 3 == 1)
+        printf(" ");
+    else
+    {
+        if (n / 3 == 0)
+            printf("*");
+        else
+            print_stars(i, j, n / 3);
+    }
 }
 
-int	main(void)
+int    main(void)
 {
-	int	n;
-
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-			print_stars(i, j, n);
-		printf("\n");
-	}
-	return (0);
+    int    n;
+    
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            print_stars(i, j, n);
+        printf("\n");
+    }
 }
