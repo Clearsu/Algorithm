084c0e8ce6a1ae203b5810dc3416cf34154dd82a
jincpark <clearwater1219@gmail.com>
2022-09-04 18:11:40 +0900

    b1920 unsolved

--- b1920_unsolved.c
+++ b1920_unsolved.c
@@ -5,27 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/11 02:47:50 by jincpark          #+#    #+#             */
-/*   Updated: 2022/08/11 03:03:05 by jincpark         ###   ########.fr       */
+/*   Created: 2022/09/04 17:39:32 by jincpark          #+#    #+#             */
+/*   Updated: 2022/09/04 17:52:50 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	*put_nums_in_array(int n)
+void	put_num_in_arr(int n, int *arr)
 {
 	int	i;
-	int	*array;
 
 	i = 0;
-	array = (int *)malloc(sizeof(int) * n);
 	while (i < n)
-		scanf("%d", &array[i++]);
-	return (array);
+	{
+		scanf("%d", &arr[i]);
+		i++;
+	}
 }
 
-void	print_nums_in_a(int n, int m, int *a)
+void	check_and_print_nums(int *arr, int n, int m)
 {
 	int	i;
 	int	j;
@@ -38,7 +38,7 @@ void	print_nums_in_a(int n, int m, int *a)
 		j = 0;
 		while (j < n)
 		{
-			if (num == a[j])
+			if (arr[j] == num)
 			{
 				printf("1\n");
 				break ;
@@ -53,14 +53,14 @@ void	print_nums_in_a(int n, int m, int *a)
 
 int	main(void)
 {
-	int	n;
-	int	m;
-	int	*a;
+	int n;
+	int m;
+	int	*arr;
 
 	scanf("%d", &n);
-	a = put_nums_in_array(n);
+	arr = (int *)malloc(sizeof(int) * n);
+	put_num_in_arr(n, arr);
 	scanf("%d", &m);
-	print_nums_in_a(n, m, a);
-	free(a);
-	return (0);
+	check_and_print_nums(arr, n, m);
+	free(arr);
 }
