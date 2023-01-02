c31bcfd46e8dcac47f8cd8c0a2bf93b135a4519e
Clearsu <clearwater1219@gmail.com>
2023-01-03 03:30:51 +0900

    need to debug

--- b2751.c
+++ b2751.c
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 01:54:16 by jincpark          #+#    #+#             */
-/*   Updated: 2023/01/03 02:53:17 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/03 03:26:44 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,24 @@ void	print_arr(int *arr, int n)
 
 void	sort_and_merge(int *arr_src, int *arr_dest, int first, int mid, int last)
 {
+	int	idx_a;
+	int	idx_b;
 	
+	idx_a = first;
+	idx_b = mid + 1;
+	for (int curr = idx_a; curr <= last; curr++)
+	{
+		if (idx_a <= mid && (idx_b > last || arr_src[idx_a] < arr_src[idx_b]))
+		{
+			arr_dest[curr] = arr_src[idx_a];
+			idx_a++;
+		}
+		else
+		{
+			arr_dest[curr] = arr_src[idx_b];
+			idx_b++;
+		}
+	}
 }
 
 void	devide_and_merge(int *arr_src, int *arr_dest, int first, int last)
@@ -55,15 +72,20 @@ int	main(void)
 	int	*arr_dest;
 
 	scanf("%d", &n);
-	if (n == 0)
-		return (0);
+	if (n <= 0)
+		return (1);
 	arr_src = read_nums(n);
 	arr_dest = calloc(n, sizeof(int));
 	if (!arr_src || !arr_dest)
 		return (1);
 	if (n > 1)
+	{
 		devide_and_merge(arr_src, arr_dest, 0, n - 1);
-	print_arr_src(arr_src, n);
+		print_arr(arr_dest, n);
+	}
+	else
+		print_arr(arr_src, n);
 	free(arr_src);
+	free(arr_dest);
 	return (0);
 }
