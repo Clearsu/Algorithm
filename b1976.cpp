2a63078b7521d995622f3ad308600ae7f6412a38
Clearsu <clearwater1219@gmail.com>
2023-11-19 15:51:43 +0900

    b1976 solved

---
+++ b1976.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() {
+	std::cin.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	int n, m;
+	char graph[201][201];
+	int plan[1001];
+
+	std::cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			std::cin >> graph[i][j];
+		}
+	}
+	for (int i = 1; i <= m; i++) {
+		std::cin >> plan[i];
+	}
+
+	for (int k = 1; k <= n; k++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if ((graph[k][i] == '1' && graph[k][j] == '1')) {
+					graph[i][j] = '1';
+					graph[j][i] = '1';
+				}
+			}
+		}
+	}
+
+	for (int i = 1; i < m; i++) {
+		if (plan[i] != plan[i + 1] && graph[plan[i]][plan[i + 1]] == '0') {
+			std::cout << "NO\n";
+			return 0;
+		}
+	}
+
+	std::cout << "YES\n";
+
+	return 0;
+}
