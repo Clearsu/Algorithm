16bc72afc64bb7654192d823b0acf80131e73500
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2024-01-04 11:27:00 +0900

    [Gold II] Title: 보석 도둑, Time: 156 ms, Memory: 8740 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+int bags[300000];
+pair<int, int> jewels[300000];
+
 int main(void) {
 	cin.tie(0);
 	ios_base::sync_with_stdio(0);
@@ -11,37 +14,31 @@ int main(void) {
 	int n, k;
 	cin >> n >> k;
 
-	priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > jewels;
-	priority_queue<int, vector<int>, greater<int> > bags;
-	
-	int weight, value;
 	for (int i = 0; i < n; i++) {
-		cin >> weight >> value;
-		jewels.push(pair<int, int>(weight, value));
+		cin >> jewels[i].first >> jewels[i].second;
 	}
+	sort(jewels, jewels + n);
 
-	int capacity;
 	for (int i = 0; i < k; i++) {
-		cin >> capacity;
-		bags.push(capacity);
+		cin >> bags[i];
 	}
+	sort(bags, bags + k);
 
-	priority_queue<pair<int, int> > pqueue;
+	priority_queue<int> jewelsToSteal;
 
+	int jewelIdx = 0;
 	long long maxValue = 0;
 	int jewelWeight, currBagCapacity;
-	while (!bags.empty()) {
-		currBagCapacity = bags.top();
-		bags.pop();
-		while (!jewels.empty() && jewels.top().first <= currBagCapacity) {
-			pqueue.push(pair<int, int>(jewels.top().second, jewels.top().first));
-			jewels.pop();
+
+	for (int i = 0; i < k; i++) {
+		while (jewelIdx < n && jewels[jewelIdx].first <= bags[i]) {
+			jewelsToSteal.push(jewels[jewelIdx].second);
+			jewelIdx++;
 		}
-		if (pqueue.empty()) {
-			continue ;
+		if (!jewelsToSteal.empty()) {
+			maxValue += jewelsToSteal.top();
+			jewelsToSteal.pop();
 		}
-		maxValue += pqueue.top().first;
-		pqueue.pop();
 	}
 
 	cout << maxValue << '\n';
