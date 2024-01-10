7fb167460acd001dc7507dd972827a272c41b2b0
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2024-01-10 18:49:33 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 208 ms, Memory: 72344 KB -BaekjoonHub

---
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m, k;
+vector<long long> tree, lazy;
+vector<long long> initialValues;
+
+long long init(int start, int end, int node) {
+	if (start == end) {
+		tree[node] = initialValues[start];
+		return tree[node];
+	}
+	int mid = (start + end) / 2;
+	return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+}
+
+void update_lazy(int start, int end, int node) {
+    if (lazy[node] != 0) {
+        tree[node] += (end - start + 1) * lazy[node];
+        if (start != end) {
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+}
+
+void update_range(int start, int end, int node, int left, int right, long long diff) {
+    update_lazy(start, end, node);
+    if (left > end || right < start) return;
+    if (left <= start && end <= right) {
+		lazy[node] = diff;
+		update_lazy(start, end, node);
+        return;
+    }
+    int mid = (start + end) / 2;
+    update_range(start, mid, node * 2, left, right, diff);
+    update_range(mid + 1, end, node * 2 + 1, left, right, diff);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+long long sum(int start, int end, int node, int left, int right) {
+    update_lazy(start, end, node);
+    if (left > end || right < start) return 0;
+    if (left <= start && end <= right) return tree[node];
+    int mid = (start + end) / 2;
+    return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m >> k;
+
+    initialValues.resize(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> initialValues[i];
+    }
+
+    tree.resize(4 * n + 1);
+    lazy.assign(4 * n + 1, 0);
+
+	init(1, n, 1);
+
+    for (int i = 0; i < m + k; i++) {
+        int operation, idx1, idx2;
+		long long diff;
+        cin >> operation;
+        if (operation == 1) {
+            cin >> idx1 >> idx2 >> diff;
+            update_range(1, n, 1, idx1, idx2, diff);
+        } else {
+            cin >> idx1 >> idx2;
+            cout << sum(1, n, 1, idx1, idx2) << '\n';
+        }
+    }
+
+    return 0;
+}
