2cfa71795efb954e59a6960dc7f871835eafb0d7
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2024-03-25 23:49:43 +0900

    [Silver V] Title: 나이순 정렬, Time: 364 ms, Memory: 25296 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -0,0 +1,7 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+input.shift();
+
+input.sort((a, b) => parseInt(a) - parseInt(b));
+
+console.log(input.join("\n"));
