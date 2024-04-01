c723edd2c3928a77c99e1d1978798785911ecd28
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2024-04-01 20:28:43 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 128 ms, Memory: 10116 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
@@ -6,16 +6,11 @@ const [firstLine, ...rest] = require("fs")
 
 const [N, M] = firstLine.split(" ").map(Number);
 
-const ans = [];
+console.log(
+  Array.from({ length: N }, (_, i) => {
+    const a = rest[i].split(" ").map(Number);
+    const b = rest[i + N].split(" ").map(Number);
 
-for (let i = 0; i < N; i++) {
-  const a = rest[i].split(" ").map(Number);
-  const b = rest[i + N].split(" ").map(Number);
-  const res = [];
-  for (let j = 0; j < M; j++) {
-    res.push(a[j] + b[j]);
-  }
-  ans.push(res);
-}
-
-console.log(ans.map((row) => row.join(" ")).join("\n"));
+    return a.map((val, idx) => val + b[idx]).join(" ");
+  }).join("\n")
+);
